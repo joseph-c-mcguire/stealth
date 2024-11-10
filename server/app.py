ce0715b38f830395b6006f15d7d65fd0079f6562
Joseph McGuire <bigmeanjoegreen@gmail.com>
2024-11-10 12:08:39 -0500

    Push build folder

--- server/app.py
+++ server/app.py
@@ -14,4 +14,4 @@ def api_example():
     return {'message': 'Hello from Flask API'}
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app = Flask(__name__, static_folder='../client/build', static_url_path='')
